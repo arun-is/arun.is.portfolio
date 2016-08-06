92b3c6f7e0e030058c6c74e621934761a76bb963
Arun Venkatesan <dallasarun@gmail.com>
2016-08-05 18:31:38 -0700

    posts, favicon, images
    -added posts, barebones template for posts
    -added some post content
    -added favicon
    -added home button (AV)
    -added images for posts

--- plugins/blog.py
+++ plugins/blog.py
@@ -34,7 +34,7 @@ def preBuild(site):
 			context_post = {"path": page.path}
 
 			# Check if we have the required keys
-			for field in ["title", "date", "tags", "thumbnail", "description"]:
+			for field in ["title", "date", "tags", "thumbnail", "description", "client", "team"]:
 				
 				if not context.has_key(field):
 					logging.warning("Page %s is missing field: %s" % (page.path, field))
